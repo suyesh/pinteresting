be9ca12b5798dd98c4a5530a2bc2822044b609fe
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-29 20:23:14 -0400

    Paperclip::Errors::MissingRequiredValidatorError in PinsController#create

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -59,6 +59,6 @@ class PinsController < ApplicationController
 
 
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
